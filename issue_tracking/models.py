aef510d838043f183d33856f2d62c451c1808697
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2021-11-26 23:10:50 +0100

    first commit

---
+++ issue_tracking/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=50)
+
+
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=500)
+    type = models.CharField(max_length=150)
+    author_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Contributor(models.Model):
+    user_id = models.ManyToManyField(User)
+    project_id = models.IntegerField()
+    role = models.CharField(max_length=150)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=150)
+    desc = models.CharField(max_length=150)
+    tag = models.CharField(max_length=150)
+    priority = models.CharField(max_length=150)
+    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
+    status = models.CharField(max_length=150)
+    author_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_issue")
+    assignee_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="assignee_issue")
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=150)
+    author_user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    issue_id = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    created_time = models.DateField(auto_now_add=True)
---
+++ issue_tracking/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework.serializers import ModelSerializer
+
+from models import User, Project, Contributor, Issue, Comment
+
