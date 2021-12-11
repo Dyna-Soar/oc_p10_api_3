c8ed3289d446a011e44cb36ccf647f6bb072f7aa
Bodin <t.bodin@MacBook-Air-de-Bodin.local>
2021-12-11 16:42:39 +0100

    adding readme

--- issue_tracking/views.py
+++ issue_tracking/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from itertools import chain
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
